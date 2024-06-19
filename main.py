2eea5f3a746f1fa54232515004d217de6084250c
KingstonS1 <159858145+KingstonS1@users.noreply.github.com>
2024-06-20 10:20:13 +1200

    define hp

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 #--------Functions -----------
-def 
+
 def intro():
     player_name = input("Hey gambler! Do you know where you are right now? Unfortuntely we can't carry on until we know your name. Can you tell us?")
     print(player_name, "? Glad you remembered. Now I should probably catch you up on recent events.. Everything's gone to absolute sh*t.")
+
+#function for health
+def hp():
+    health = 5
+    if health < 0:
+    score = 395
 
 # ----------- Main Code --------------
 
