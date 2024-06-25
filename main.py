7158b30a1bf444095206cb73879092c7015ef2e1
EvieLupine <159732957+EvieLupine@users.noreply.github.com>
2024-06-25 15:06:15 +1200

    Save

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def intro():
     player_name = input("Hey gambler! Do you know where you are right now? Unfortuntely we can't carry on until we know your name. Can you tell us?")
     print(player_name, "? Glad you remembered. Now I should probably catch you up on recent events.. Everything's gone to absolute sh*t.")
     print("After spending your childs college fund (and losing), you are now stuck inside a Casino. Unfortunetly you find yourself trying to gamble back your debt with a few minor family relics.")
-    print("")
+    print("Since you've woken up, it becomes apparent that the situation you're in is dire. Civilisations accross the globe have collapsed, torn apart and simply forgotten. Checking in on yourself you see that you are fine for now but everyone around you is mostly sick and infected with a virus called RAV26 that has inhereted most of the world. You decide to set off on foot to try find a way to make it all better. ")
 
 #function for health
 def hp():
